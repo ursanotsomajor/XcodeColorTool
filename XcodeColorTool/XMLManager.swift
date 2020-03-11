728e4ceb697209621d14010f8bbdea8faa02753f
Eduard Lisin <ursanotsomajor@gmail.com>
2020-03-11 08:29:31 +0500

    Parsing colors

--- XcodeColorTool/XMLManager.swift
+++ XcodeColorTool/XMLManager.swift
@@ -9,7 +9,33 @@ class XMLManager: NSObject, ObservableObject
 {
     @Published var state = XMLManagerState.waiting
     
-    func load(url: URL)
+    func onDrop(info: DropInfo) -> Bool
+    {
+        let id = kUTTypeFileURL as String
+        
+        guard let itemProvider = info.itemProviders(for: [id]).first else { return false }
+        
+        itemProvider.loadItem(forTypeIdentifier: id, options: nil) { item, error in
+            guard let data = item as? Data, let url = URL(dataRepresentation: data, relativeTo: nil) else { return }
+            self.load(url: url)
+        }
+        
+        return true
+    }
+    
+    func replaceColors(operation: ColorReplacementOperation)
+    {
+        for file in operation.selectedFiles
+        {
+            parse(file: file, replacements: operation.colorReplacements)
+        }
+    }
+}
+
+// MARK: Files
+extension XMLManager
+{
+    private func load(url: URL)
     {
         print("Reading files at: \(url.path)\n")
         
@@ -18,20 +44,6 @@ class XMLManager: NSObject, ObservableObject
         }
         
         listFiles(url: url)
-    }
-    
-    func onDrop(info: DropInfo) -> Bool
-    {
-        let id = kUTTypeFileURL as String
-        
-        guard let itemProvider = info.itemProviders(for: [id]).first else { return false }
-
-        itemProvider.loadItem(forTypeIdentifier: id, options: nil) { item, error in
-            guard let data = item as? Data, let url = URL(dataRepresentation: data, relativeTo: nil) else { return }
-            self.load(url: url)
-        }
-        
-        return true
     }
     
     private func listFiles(url: URL)
@@ -73,27 +85,123 @@ class XMLManager: NSObject, ObservableObject
             self.state = .presenting(operation: ColorReplacementOperation(files: models))
         }
     }
-    
-    func replaceColors(operation: ColorReplacementOperation)
-    {
-        for file in operation.selectedFiles
-        {
-            rewrite(file: file, replacements: operation.replacements)
-        }
-    }
 }
 
-extension XMLManager: XMLParserDelegate
+
+// MARK: Parsing
+extension XMLManager
 {
-    private func rewrite(file: FileModel, replacements: [ColorReplacementModel])
+    private func parse(file: FileModel, replacements: [ColorReplacementModel])
     {
         do {
             let data = try Data(contentsOf: file.url)
-            let xmlDoc = try AEXMLDocument(xml: data, options: AEXMLOptions())
-            print(xmlDoc.xml)
+            let xmlDocument = try AEXMLDocument(xml: data, options: AEXMLOptions())
+            
+//            print("Parsing: \(file.name)\n")
+            
+            // Adding Named Colors dependency if there is none
+            let dependencies = xmlDocument.root["dependencies"]
+            
+            if !dependencies.children.contains(where: { $0.attributes["name"] == "Named colors" })
+            {
+                let attributes = ["name": "Named colors", "minToolsVersion": "9.0"]
+                dependencies.addChild(AEXMLElement(name: "capability", value: nil, attributes: attributes))
+            }
+            
+            
+            //Replacing all exact or similar colors within delta
+            let colorElements = extractColorElements(in: xmlDocument.root)
+            
+            for item in colorElements
+            {
+                replaceColor(in: item, with: replacements)
+            }
+            
+            
+            // Adding color resources
+            let resources: AEXMLElement
+            
+            if let error = xmlDocument.root["resources"].error, error == .elementNotFound
+            {
+                resources = AEXMLElement(name: "resources")
+                xmlDocument.root.addChild(resources)
+            }
+            else {
+                resources = xmlDocument.root["resources"]
+            }
+            
+            for (color, name) in replacements.map({ ($0.customColor, $0.name) })
+            {
+                resources.children.removeAll(where: { $0.attributes["name"] == name })
+                
+                let namedColor = AEXMLElement(name: "namedColor", value: nil, attributes: ["name": name])
+                let colorAttributes = ["red": "\(color.r)", "green": "\(color.g)", "blue": "\(color.b)", "alpha": "\(color.a)", "colorSpace": "custom", "customColorSpace": "sRGB"]
+                namedColor.addChild(name: "color", value: nil, attributes: colorAttributes)
+                resources.addChild(namedColor)
+            }
         }
         catch {
             print(error)
+        }
+    }
+    
+    private func extractColorElements(in element: AEXMLElement) -> [AEXMLElement]
+    {
+        var colorElements = [AEXMLElement]()
+        
+        for item in element.children
+        {
+            if item.children.isEmpty
+            {
+                if item.name == "color"
+                {
+                    colorElements.append(item)
+                }
+            }
+            else
+            {
+                colorElements.append(contentsOf: extractColorElements(in: item))
+            }
+        }
+        
+        return colorElements
+    }
+    
+    private func replaceColor(in element: AEXMLElement, with replacements: [ColorReplacementModel])
+    {
+        let attributes = element.attributes
+        
+        if let whiteString = attributes["white"], let white = Double(whiteString)
+        {
+//            let customColorSpace = attributes["customColorSpace"]
+//            let colorSpace = attributes["colorSpace"]
+//            let alphaString = attributes["alpha"]
+//            let alpha = Double(alphaString)
+//            let key = attributes["key"]
+        }
+        else if let redString = attributes["red"], let red = Double(redString),
+            let greenString = attributes["green"], let green = Double(greenString),
+            let blueString = attributes["blue"], let blue = Double(blueString)
+        {
+//            let customColorSpace = attributes["customColorSpace"]
+//            let colorSpace = attributes["colorSpace"]
+//            let alphaString = attributes["alpha"]
+//            let alpha = Double(alphaString)
+//            let key = attributes["key"]
+        }
+        else if let cocoaTouchSystemColor = attributes["cocoaTouchSystemColor"], let key = attributes["key"]
+        {
+//            let systemColor = attributes["systemColor"]
+        }
+        else if let key = attributes["key"], let name = attributes["name"]
+        {
+
+        }
+        else
+        {
+            print("Couldn't parse")
+            print(element.attributes)
+            print(element.xml)
         }
     }
 }
